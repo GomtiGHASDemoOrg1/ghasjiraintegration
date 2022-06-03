d53a221554db8e380bf81a08655a9cc45491d437
Dylan Rinker <dylan-rinker@github.com>
2022-06-03 17:34:13 +0000

    push protection

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const apiKey = "12345678901234567890123456789012-12345678-12345678";
 const db = "abc123.database.secure.windows.net"
 
+STRIPE_SECRET_KEY = 'sk_live_abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789a'
+
+
 function endsWith(x, y) {
   return x.lastIndexOf(y) === x.length - y.length;
 }
